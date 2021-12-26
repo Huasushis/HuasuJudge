f0e80698206c3c724a880b0116efc0b61eb5fb3c
huasushis <huasushi@outlook.com>
2021-12-26 21:44:36 +0800

    添加项目文件。

---
+++ HuasuJudge/HuasuJudge.h
@@ -0,0 +1,32 @@
+﻿
+// HuasuJudge.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CHuasuJudgeApp:
+// 有关此类的实现，请参阅 HuasuJudge.cpp
+//
+
+class CHuasuJudgeApp : public CWinApp
+{
+public:
+	CHuasuJudgeApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CHuasuJudgeApp theApp;
